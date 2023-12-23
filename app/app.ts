1e4ea8e833c9cb96f79eed26a3c14ef8aa115b14
Miguel Muller <miguelsmuller@users.noreply.github.com>
2023-12-23 10:08:58 -0300

    chore: resolve ESLint warnings and errors

--- app/app.ts
+++ app/app.ts
@@ -1,30 +1,30 @@
-import express, { Router, Request, Response } from 'express'
-import cors from 'cors';
+import express, { Router, Request, Response } from "express";
+import cors from "cors";
 
-const app = express()
-const route = Router()
+const app = express();
+const route = Router();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-route.get('/health', (req: Request, res: Response) => {
-    const health = {
-        "message": "Welcome to Express and PostgreSQL",
-        "status": "success"
-    }; 
-    res.json(health)
-})
+route.get("/health", (_req: Request, res: Response) => {
+  const health = {
+    message: "Welcome to Express and PostgreSQL",
+    status: "success",
+  };
+  res.json(health);
+});
 
-route.get('/getAll', (req: Request, res: Response) => {
-    const health = {
-        "message": "Welcome to Express and PostgreSQL",
-        "status": "success"
-    }; 
-    res.json(health)
-})
+route.get("/getAll", (_req: Request, res: Response) => {
+  const health = {
+    message: "Welcome to Express and PostgreSQL",
+    status: "success",
+  };
+  res.json(health);
+});
 
-app.use(route)
+app.use(route);
 
 app.listen(3000, () => {
-    console.log("Server Listening on PORT: 3000");
+  console.log("Server Listening on PORT: 3000");
 });
